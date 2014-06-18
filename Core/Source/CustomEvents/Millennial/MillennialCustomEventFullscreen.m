d006310de46a1af3ececac6222d9909d22618fdf
Michał Kapuściński <michal.kapuscinski@gecko-lab.com>
2014-06-18 12:59:04 +0200

    Initial Millennial banner and fullscreen custom events implementation.

---
+++ Core/Source/CustomEvents/Millennial/MillennialCustomEventFullscreen.m
@@ -0,0 +1,98 @@
+//
+//  MillennialCustomEventFullscreen.m
+//  MobFoxSDKSource
+//
+//  Created by Michał Kapuściński on 18.06.2014.
+//
+//
+
+#import "MillennialCustomEventFullscreen.h"
+
+#define COMPILE_MILLENNIAL_ADDITIONAL_NOTIFICATIONS 0 //disabled, would cause errors without Millennial framework attached. Basic delegate methods (ad loaded/failed to load) will still work
+
+@interface MillennialCustomEventFullscreen()
+@property (nonatomic, strong) NSString* adId;
+@end
+
+
+@implementation MillennialCustomEventFullscreen
+
+- (void)loadFullscreenWithOptionalParameters:(NSString *)optionalParameters trackingPixel:(NSString *)trackingPixel
+{
+    self.trackingPixel = trackingPixel;
+    self.adId = optionalParameters;
+    
+    Class interstitialClass = NSClassFromString(@"MMInterstitial");
+    Class requestClass = NSClassFromString(@"MMRequest");
+    Class SDKClass = NSClassFromString(@"MMSDK");
+    if(!interstitialClass || !requestClass || SDKClass) {
+        [self.delegate customEventFullscreenDidFailToLoadAd];
+        return;
+    }
+    [SDKClass initialize];
+    
+#if COMPILE_MILLENNIAL_ADDITIONAL_NOTIFICATIONS
+
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(adModalWillDismiss:)
+                                                 name:MillennialMediaAdModalWillDismiss
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationWillTerminateFromAd:)
+                                                 name:MillennialMediaAdWillTerminateApplication
+                                               object:nil];
+    
+#endif
+
+    MMRequest *request = [requestClass request];
+    
+    [interstitialClass fetchWithRequest:request
+                                apid:optionalParameters
+                        onCompletion:^(BOOL success, NSError *error) {
+                            if (success) {
+                                [self.delegate customEventFullscreenDidLoadAd:self];
+                            }
+                            else {
+                                [self.delegate customEventFullscreenDidFailToLoadAd];
+                            }
+                        }];
+    
+}
+
+- (void)showFullscreenFromRootViewController:(UIViewController *)rootViewController
+{
+    Class interstitialClass = NSClassFromString(@"MMInterstitial");
+    if(interstitialClass && [interstitialClass isAdAvailableForApid:self.adId]) {
+        [self didDisplayAd];
+        [self.delegate customEventFullscreenWillAppear];
+        
+        [interstitialClass displayForApid: self.adId
+                 fromViewController: rootViewController
+                    withOrientation: 0
+                       onCompletion: nil];
+    }
+}
+
+
+- (void)adModalWillDismiss:(NSNotification *)notification {
+    [self.delegate customEventFullscreenWillClose];
+}
+
+- (void)applicationWillTerminateFromAd:(NSNotification *)notification {
+    [self.delegate customEventFullscreenWillLeaveApplication];
+}
+
+
+- (void)dealloc
+{
+#if COMPILE_MILLENNIAL_ADDITIONAL_NOTIFICATIONS
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+#endif
+}
+
+
+
+
+@end
