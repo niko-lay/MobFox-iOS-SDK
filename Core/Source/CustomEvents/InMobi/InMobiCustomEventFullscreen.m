7ccce8b32fbe5849ef273f20e11de20bc116820f
Michał Kapuściński <michal.kapuscinski@gecko-lab.com>
2014-07-08 13:21:10 +0200

    Add InMobi custom events for banners and interstitial ads.

---
+++ Core/Source/CustomEvents/InMobi/InMobiCustomEventFullscreen.m
@@ -0,0 +1,68 @@
+//
+//  InMobiCustomEventFullscreen.m
+//  MobFoxSDKSource
+//
+//  Created by Michał Kapuściński on 08.07.2014.
+//
+//
+
+#import "InMobiCustomEventFullscreen.h"
+
+@implementation InMobiCustomEventFullscreen
+
+- (void)loadFullscreenWithOptionalParameters:(NSString *)optionalParameters trackingPixel:(NSString *)trackingPixel
+{
+    self.trackingPixel = trackingPixel;
+    
+    Class interstitialClass = NSClassFromString(@"IMInterstitial");
+    if(!interstitialClass) {
+        [self notifyAdFailed];
+        return;
+    }
+    
+    interstitial = [[interstitialClass alloc]initWithAppId:optionalParameters];
+    interstitial.delegate = self;
+    [interstitial loadInterstitial];
+    
+    
+}
+
+- (void)showFullscreenFromRootViewController:(UIViewController *)rootViewController
+{
+    [interstitial presentInterstitialAnimated:YES];
+}
+
+- (void)dealloc
+{
+    interstitial.delegate = nil;
+    interstitial = nil;
+}
+
+#pragma mark - Delegate methods
+-(void)interstitial:(IMInterstitial *)ad didFailToReceiveAdWithError:(IMError *)error {
+    [self notifyAdFailed];
+}
+
+-(void)interstitialDidDismissScreen:(IMInterstitial *)ad {
+    [self notifyAdWillClose];
+}
+
+-(void)interstitialDidReceiveAd:(IMInterstitial *)ad {
+    [self notifyAdLoaded];
+}
+
+-(void)interstitialWillLeaveApplication:(IMInterstitial *)ad {
+    [self notifyAdWillLeaveApplication];
+}
+
+-(void)interstitialWillPresentScreen:(IMInterstitial *)ad {
+    [self notifyAdWillAppear];
+}
+
+-(void)interstitial:(IMInterstitial *)ad didFailToPresentScreenWithError:(IMError *)error {
+    [self notifyAdWillClose];
+}
+
+
+
+@end
