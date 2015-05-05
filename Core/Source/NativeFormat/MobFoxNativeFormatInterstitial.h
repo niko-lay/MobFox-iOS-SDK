ceae5d869ca4f16727ccafb94e081a1e52fc39ac
Michał Kapuściński <michal.kapuscinski@gecko-lab.com>
2015-05-05 12:49:04 +0200

    Initial implementation of Native Format Interstitial.

---
+++ Core/Source/NativeFormat/MobFoxNativeFormatInterstitial.h
@@ -0,0 +1,31 @@
+//
+//  MobFoxNativeFormatInterstitial.h
+//  MobFoxSDKSource
+//
+//  Created by Michał Kapuściński on 05.05.2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol MobFoxNativeFormatInterstitialDelegate <NSObject>
+
+- (void)mobfoxNativeFormatInterstitialDidLoad;
+
+- (void)mobfoxNativeFormatInterstitialDidFailToLoadWithError:(NSError *)error;
+
+- (void)mobfoxNativeFormatInterstitialWillPresent;
+
+- (void)mobfoxNativeFormatInterstitialActionWillFinish;
+
+@end
+
+@interface MobFoxNativeFormatInterstitial : NSObject
+
+@property (nonatomic, assign) IBOutlet __unsafe_unretained id <MobFoxNativeFormatInterstitialDelegate> delegate;
+
+-(void) requestAdWithPublisherId:(NSString *)publisherId andViewController:(UIViewController*)controller;
+
+-(void) showAd;
+
+@end
